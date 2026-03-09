b8931be5d0a2c786fd553300858ed7fe497c09fd
Priyanka Srivastava <srivastavapriyanka71307@gmail.com>
2026-03-10 00:50:35 +0530

    updated resume

--- src/react-app/components/About.tsx
+++ src/react-app/components/About.tsx
@@ -41,7 +41,7 @@ export default function About() {
           >
             About <span className="text-gradient">Me</span>
           </h2>
-
+         {/* profile */}
           <div className="flex justify-center">
   <div className="w-56 h-56 md:w-64 md:h-64 rounded-full border-4 border-purple-500/40 shadow-[0_0_40px_rgba(168,85,247,0.3)] overflow-hidden">
     <img
